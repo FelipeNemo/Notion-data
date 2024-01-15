c7ce7c8e0cc5e09638d8950b01832af243478948
Felipe Augusto <felipedata20@gmail.com>
2024-01-15 20:03:13 -0300

    Lógica para inserir dados na pasta notion

---
+++ index.js
@@ -0,0 +1,82 @@
+// As propriedades do dataset(coluna) devem se conectar com as propriedades da página(linha)
+
+import { Client } from "@notionhq/client"
+import { config } from "dotenv"
+import { propertiesForNewPages } from ".sampleData.js"
+
+config()
+
+const pageId = process.env.NOTION_PAGE_ID
+const apiKey = process.env.NOTION_API_KEY
+
+const notion = new Client({ auth: apiKey })
+
+
+
+//CRIA A PASTA
+//No Banco de dados contido na página pai(Navegador>Página Pai>Página FIlha>Olhos do Usuário)
+async function AdicionandoPaginaAoBancoDeDados(databaseId, pageProperties) {   
+  const newPage = await notion.pages.create({
+    parent: { //Pagina pai
+      database_id: databaseId,//Id do pai
+    },
+    properties: pageProperties,//Propriedade da página filha
+  })
+  console.log(newPage)// print a newPage
+}
+
+
+//Criação do Banco de dados dentro da Pasta
+async function main() {
+  const newDatabase = await notion.databases.create({
+    parent: { //Página filha cria um filho
+      type: "page_id", // O tipo do filho É page_id  que demonstra o TITULO
+      page_id: pageId,
+    },
+    title: [ //Titulo fora e dentro da Pasta
+      {
+        type: "text",
+        text: {
+          content: "Grocery item",//Conteudo do titulo
+        },
+      },
+    ],
+    properties: { 
+      // Propriedade do dataset notion(coluna)
+      "Grocery item": {
+        type: "title",
+        title: {},
+      },
+      Price: {
+        type: "number",
+        number: {
+          format: "dollar",
+        },
+      },
+      "Last ordered": {
+        type: "date",
+        date: {},
+      },
+    },
+  })
+
+  // Imprime a URL do novo banco de dados. Visite o URL em seu navegador para ver as páginas criadas na próxima etapa.
+  console.log(newDatabase.url)//printa a url no terminal
+
+  //Tratamento de Erro: SE HOUVER ERRO DE CONEXÃO DA URL
+  const databaseId = newDatabase.id
+ // Se não houver ID (se houver erro), retorne.
+  if (!databaseId) return ;//databaseId: Verifica se databaseId é falso, nulo ou indefinido.
+                           //return: Se a condição for verdadeira (ou seja, databaseId é falso,
+                           // nulo ou indefinido), a função atual será interrompida e retornará imediatamente.
+
+  // propertiesForNewPages é um array(vetor) que exportamos do módulo sampleData.js
+  // Ele guarda as propriedades das novas pastas a serem criadas 
+  console.log("Adicionando novas páginas....")
+  for (let i = 0; i < propertiesForNewPages.length; i++) {
+  //Adiciona algumas páginas novas ao banco de dados que acabou de ser criado
+    await AdicionandoPaginaAoBancoDeDados(databaseId, propertiesForNewPages[i])
+  }
+}
+
+main()
---
+++ sampleData.js
@@ -0,0 +1,47 @@
+//Importações de modulos
+import fs from 'fs';// para analisar arquivos CSV
+import csv from 'csv-parser'// para analisar arquivos CSV
+
+// Nome do arquivo CSV no mesmo diretório
+const ArquivoCSV = 'GroceryItem.csv';
+
+// Colocando tudo dentro do vetor propertiesForNewPages
+export const propertiesForNewPages = [];
+
+// Array para armazenar os dados do CSV
+const dadosCSV = [];
+
+// Ler o arquivo CSV e transformar em objeto JSON
+fs.createReadStream(ArquivoCSV)
+  .pipe(csv())
+  .on('data', (linha) => {
+    // Adicionar cada linha do CSV ao array de dadosCSV
+    dadosCSV.push(linha);
+  })
+  .on('end', () => {
+    // Mapear os dadosCSV para o formato desejado
+  const propriedadesAtualizadas = dadosCSV.map((item) => {
+      return {  //Propriedades da pasta notion(linhas)
+        "Grocery item": {
+          type: "title",
+          title: [{ type: "text", text: { content: item["Grocery item"] } }],
+
+          Price: {
+            type: "number",
+            number: parseFloat(item.Price), // Converter para número
+          },
+
+          "Last ordered": {
+            type: "date",
+            date: { start: item["Last ordered"] },
+          },
+        },
+      };
+    }); 
+    
+    // "..." concatena arrays
+    propertiesForNewPages.push(...propriedadesAtualizadas);//Coloca as propriedades da página dentro do vetor que exportamos
+    //console.log(propriedadesAtualizadas)
+    console.log(propriedadesAtualizadas);//Coloca as propriedades da página dentro do vetor que exportamos
+    
+  });
